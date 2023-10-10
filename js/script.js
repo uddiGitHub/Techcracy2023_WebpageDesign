9f59d7637eba34305d784c7091ad62fac0cc7d91
uddiGitHub <u20cse1071@cit.ac.in>
2023-10-11 02:04:57 +0530

    initials

---
+++ js/script.js
@@ -0,0 +1,152 @@
+function loco() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
+loco();
+
+// canvas
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    render();
+});
+
+// function files(index) {
+//     path = "images/"
+//     var data = `
+//     ${path}frame_0000.png
+//     ${path}frame_0001.png
+//     ${path}frame_0002.png
+//     ${path}frame_0003.png
+//     ${path}frame_0004.png
+//     ${path}frame_0005.png
+//     ${path}frame_0006.png
+//     ${path}frame_0007.png
+//     ${path}frame_0008.png
+//     ${path}frame_0009.png
+//     ${path}frame_0010.png
+//     ${path}frame_0011.png
+//     ${path}frame_0012.png
+//     ${path}frame_0013.png
+//     ${path}frame_0014.png
+//     ${path}frame_0015.png
+//     ${path}frame_0016.png
+//     ${path}frame_0017.png
+//     ${path}frame_0018.png
+//     ${path}frame_0019.png
+//     ${path}frame_0020.png
+//     ${path}frame_0021.png
+//     ${path}frame_0022.png
+//     ${path}frame_0023.png
+//     ${path}frame_0024.png
+//     ${path}frame_0025.png
+//     ${path}frame_0026.png
+//  `;
+//     return data.split("\n")[index];
+// }
+function files(index) {
+    const path = "images/background/";
+    return `${path}${index.toString().padStart(4, '0')}.png`;
+}
+
+
+const frameCount = 110;
+
+const images = [];
+const imageSeq = {
+    frame: 1,
+};
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = files(i);
+    images.push(img);
+}
+
+gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: `none`,
+    scrollTrigger: {
+        scrub: 3,
+        trigger: `#home`,
+        //   set start end according to preference
+        start: `top top`,
+        end: `600% top`,
+        scroller: `#main`,
+    },
+    onUpdate: render,
+});
+
+images[1].onload = render;
+
+function render() {
+    scaleImage(images[imageSeq.frame], context);
+}
+
+function scaleImage(img, ctx) {
+    var canvas = ctx.canvas;
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.max(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width * ratio) / 2;
+    var centerShift_y = (canvas.height - img.height * ratio) / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+        img,
+        0,
+        0,
+        img.width,
+        img.height,
+        centerShift_x,
+        centerShift_y,
+        img.width * ratio,
+        img.height * ratio
+    );
+}
+ScrollTrigger.create({
+
+    trigger: "#home",
+    pin: true,
+    // markers:true,
+    scroller: `#main`,
+    //   set start end according to preference
+    start: `top top`,
+    end: `600% top`,
+});
