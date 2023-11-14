1749d3e4d794097f53ad48c0b6f127883603ebb8
uddiGitHub <u20cse1071@cit.ac.in>
2023-11-14 12:33:48 +0530

    scrolltypo

--- js/scrollAnimateText.js
+++ js/scrollAnimateText.js
@@ -1,40 +1,57 @@
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const splitTypes = document.querySelectorAll('.reveal-type')
 
 splitTypes.forEach((char) => {
-    const text = new SplitType(char, { type: 'chars'})
+    const text = new SplitType(char, { type: 'words' });
 
-    gsap.from(text.words, {
-        scrollTrigger: ".reveal-type",
-        // scrollTrigger: {
-        //     trigger: '.reveal-type',
-        //     // endTrigger: char,
-        //     start: 'bottom',
-        //     // end: 'top 20%',
-        //     scrub: true,
-        //     markers: true
-        // },
-        y: 100,
+    const words = [...text.words];
+    words.forEach(word => gsap.set(word.parentNode, { perspective: 1000 }));
+
+    gsap.fromTo(words, {
+        'will-change': 'opacity, transform',
+        z: () => gsap.utils.random(500, 950),
         opacity: 0,
-        stagger: 0.01
-    })
-})
+        xPercent: () => gsap.utils.random(-100, 100),
+        yPercent: () => gsap.utils.random(-10, 10),
+        rotationX: () => gsap.utils.random(-90, 90)
+    },
+    {
+        ease: 'expo',
+        opacity: 1,
+        rotationX: 0,
+        rotationY: 0,
+        xPercent: 0,
+        yPercent: 0,
+        z: 0,
+        scrollTrigger: {
+            trigger: char,
+            start: 'center center',
+            end: '+=300%',
+            scrub: 2,
+            pin: char.parentNode,
+        },
+        stagger: {
+            each: 0.006,
+            from: 'random'
+        }
+    });
+});
 
 
 
 
 
 // smooth scroll
-// const lenis = new Lenis()
+const lenis = new Lenis()
 
-// lenis.on('scroll', (e) => {
-//   console.log(e)
-// })
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
 
-// function raf(time) {
-//   lenis.raf(time)
-//   requestAnimationFrame(raf)
-// }
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
 
-// requestAnimationFrame(raf)
+requestAnimationFrame(raf)
