2b0f4a663b03243a03263428aeb552491fc383db
uddiGitHub <u20cse1071@cit.ac.in>
2023-11-16 12:38:06 +0530

    optimized

--- js/script.js
+++ js/script.js
@@ -1,123 +1,110 @@
-function loco() {
-    gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-    const locoScroll = new LocomotiveScroll({
+function initLocomotiveScroll() {
+    const locomotiveScroll = new LocomotiveScroll({
         el: document.querySelector("#main"),
         smooth: true
     });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-    locoScroll.on("scroll", ScrollTrigger.update);
 
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+    locomotiveScroll.on("scroll", ScrollTrigger.update);
+
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
-            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+            return arguments.length ? locomotiveScroll.scrollTo(value, 0, 0) : locomotiveScroll.scroll.instance.scroll.y;
+        },
         getBoundingClientRect() {
             return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
         },
-        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
         pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
     });
 
-
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.addEventListener("refresh", () => locomotiveScroll.update());
     ScrollTrigger.refresh();
-
 }
-loco();
 
-// canvas
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-canvas.style.pointerEvents = "none";
+function initCanvasAnimation() {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    canvas.style.pointerEvents = "none";
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-
-window.addEventListener("resize", function () {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    render();
-});
 
+    window.addEventListener("resize", function () {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        render();
+    });
 
-function files(index) {
-    const path = "images/comic/";
-    return `${path}${index.toString().padStart(4, '0')}.png`;
+    const frameCount = 110;
+    const images = [];
+    const imageSeq = { frame: 1 };
+
+    function files(index) {
+        const path = "images/comic/";
+        return `${path}${index.toString().padStart(4, '0')}.png`;
+    }
+
+    for (let i = 1; i <= frameCount; i++) {
+        const img = new Image();
+        img.src = files(i);
+        images.push(img);
+    }
+
+    gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: `none`,
+        scrollTrigger: {
+            scrub: true,
+            trigger: `#home>canvas`,
+            start: `top top`,
+            end: `600% top`,
+            scroller: `#main`,
+        },
+        onUpdate: render,
+    });
+
+    images[1].onload = render;
+
+    function render() {
+        scaleImage(images[imageSeq.frame], context);
+    }
+
+    function scaleImage(img, ctx) {
+        var canvas = ctx.canvas;
+        var hRatio = canvas.width / img.width;
+        var vRatio = canvas.height / img.height;
+        var ratio = Math.max(hRatio, vRatio);
+        var centerShift_x = (canvas.width - img.width * ratio) / 2;
+        var centerShift_y = (canvas.height - img.height * ratio) / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(
+            img,
+            0,
+            0,
+            img.width,
+            img.height,
+            centerShift_x,
+            centerShift_y,
+            img.width * ratio,
+            img.height * ratio
+        );
+    }
 }
 
-
-const frameCount = 110;
-
-const images = [];
-const imageSeq = {
-    frame: 1,
-};
-
-for (let i = 1; i <= frameCount; i++) {
-    const img = new Image();
-    img.src = files(i);
-    images.push(img);
-}
-
-gsap.to(imageSeq, {
-    frame: frameCount - 1,
-    snap: "frame",
-    ease: `none`,
-    scrollTrigger: {
-        scrub: true,
-        trigger: `#home>canvas`,
-        //   set start end according to preference
+function initCanvasPin() {
+    ScrollTrigger.create({
+        trigger: "#home>canvas",
+        pin: true,
         start: `top top`,
         end: `600% top`,
         scroller: `#main`,
-    },
-    onUpdate: render,
-});
-
-
-
-images[1].onload = render;
-
-function render() {
-    scaleImage(images[imageSeq.frame], context);
+    });
 }
 
-function scaleImage(img, ctx) {
-    var canvas = ctx.canvas;
-    var hRatio = canvas.width / img.width;
-    var vRatio = canvas.height / img.height;
-    var ratio = Math.max(hRatio, vRatio);
-    var centerShift_x = (canvas.width - img.width * ratio) / 2;
-    var centerShift_y = (canvas.height - img.height * ratio) / 2;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(
-        img,
-        0,
-        0,
-        img.width,
-        img.height,
-        centerShift_x,
-        centerShift_y,
-        img.width * ratio,
-        img.height * ratio
-    );
-}
-ScrollTrigger.create({
-
-    trigger: "#home>canvas",
-    pin: true,
-    // markers:true,
-    scroller: `#main`,
-    //   set start end according to preference
-    start: `top top`,
-    end: `600% top`,
+document.addEventListener("DOMContentLoaded", () => {
+    initLocomotiveScroll();
+    initCanvasAnimation();
+    initCanvasPin();
 });
